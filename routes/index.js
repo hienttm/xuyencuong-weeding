62c2cb5c0ab0793c0038917c01b7ce2f2ea7629e
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-10-17 14:46:01 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fs = require('fs');
+var fs = require("fs");
 
 /* GET all wish. */
-router.get('/listWish', function (req, res, next) {
-  let data = fs.readFileSync('./data.JSON');
+router.get("/listWish", function (req, res, next) {
+  let data = fs.readFileSync("./data.JSON");
   data = JSON.parse(data);
   res.send(data);
 });
 
 //Save a wish
-router.post('/sendWish', function (req, res, next) {
+router.post("/sendWish", function (req, res, next) {
   let newWish = req.body;
-  let data = fs.readFileSync('./data.JSON');
+  let data = fs.readFileSync("./data.JSON");
   data = JSON.parse(data);
   data.unshift(newWish);
   data = JSON.stringify(data);
-  fs.writeFileSync('data.JSON', data);
+  fs.writeFileSync("data.JSON", data);
+});
+//delete lời chúc
+router.get("/guest/delete/:name", function (req, res, next) {
+  let name = req.params.name;
+  let data = fs.readFileSync("./data.JSON");
+  data = JSON.parse(data);
+  data = data.filter((item) => {
+    return item.name != name;
+  });
+  data = JSON.stringify(data);
+  fs.writeFileSync("guest.JSON", data);
+  res.redirect("/guest");
 });
 
 //Khách mời
-router.get('/guest', function (req, res, next) {
-  let data = fs.readFileSync('./guest.JSON');
+router.get("/guest", function (req, res, next) {
+  let data = fs.readFileSync("./guest.JSON");
   data = JSON.parse(data);
-  res.render('guest/index.ejs', { guests: data });
+  res.render("guest/index.ejs", { guests: data });
 });
-
-router.post('/guest/add', function (req, res, next) {
+//getjsonguest
+router.get("/guestjson", function (req, res, next) {
+  let data = fs.readFileSync("./guest.JSON");
+  data = JSON.parse(data);
+  res.send(data);
+});
+router.post("/guest/add", function (req, res, next) {
   let newGuest = req.body;
-  let data = fs.readFileSync('./guest.JSON');
+  let data = fs.readFileSync("./guest.JSON");
   data = JSON.parse(data);
   let length = data.length + 1;
   newGuest.id = length;
   data.push(newGuest);
   data = JSON.stringify(data);
-  fs.writeFileSync('guest.JSON', data);
-  res.redirect('/guest');
+  fs.writeFileSync("guest.JSON", data);
+  res.redirect("/guest");
 });
 
-router.post('/guest/edit/:id', function (req, res, next) {
+router.post("/guest/edit/:id", function (req, res, next) {
   let id = req.params.id;
-  let data = fs.readFileSync('./guest.JSON');
+  let data = fs.readFileSync("./guest.JSON");
   data = JSON.parse(data);
   const updatedData = data.map((item) => {
     if (item.id == id) {
@@ -50,28 +67,28 @@ router.post('/guest/edit/:id', function (req, res, next) {
     return item;
   });
   data = JSON.stringify(data);
-  fs.writeFileSync('guest.JSON', data);
-  res.redirect('/guest');
+  fs.writeFileSync("guest.JSON", data);
+  res.redirect("/guest");
 });
 
-router.get('/guest/delete/:id', function (req, res, next) {
+router.get("/guest/delete/:id", function (req, res, next) {
   let id = req.params.id;
-  let data = fs.readFileSync('./guest.JSON');
+  let data = fs.readFileSync("./guest.JSON");
   data = JSON.parse(data);
   data = data.filter((item) => {
     return item.id != id;
   });
   data = JSON.stringify(data);
-  fs.writeFileSync('guest.JSON', data);
-  res.redirect('/guest');
+  fs.writeFileSync("guest.JSON", data);
+  res.redirect("/guest");
 });
 
 /* GET home page. */
-router.get('/:id?', function (req, res, next) {
+router.get("/:id?", function (req, res, next) {
   var id = req.params.id;
   var guest = null;
   if (id != null) {
-    let data = fs.readFileSync('./guest.JSON');
+    let data = fs.readFileSync("./guest.JSON");
     data = JSON.parse(data);
     for (const element of data) {
       if (element.id == id) {
@@ -80,7 +97,7 @@ router.get('/:id?', function (req, res, next) {
       }
     }
   }
-  res.render('index', { guest: guest });
+  res.render("index", { guest: guest });
 });
 
 module.exports = router;
